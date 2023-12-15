9b0c47ba8115606ab8a4d35373f30d312335c6cd
takaaki <iwamoto.takaaki@gmail.com>
2023-12-15 15:49:15 +0000

    勝敗判定

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -32,6 +32,8 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[i]) return
+    
     squares[i] = this.state.xIsNext? 'X': 'O'
     this.setState({squares: squares, xIsNext: !this.state.xIsNext})
   }
@@ -44,11 +46,19 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext? 'X': 'O');
+    const status = () => {
+      const winner = calculateWinner(this.state.squares)
+  
+      if (winner) {
+        return 'Winner: ' + winner
+      } else {
+        return 'Next player: ' + (this.state.xIsNext? 'X': 'O');
+      }
+    }
 
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status">{status()}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -89,3 +99,23 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
